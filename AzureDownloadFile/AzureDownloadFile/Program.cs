82b429a8eb1cadcc6d6b581b924d45ccf8564fc5
Hari Dileepkumar <Hari.Dileepkumar@neudesic.com>
2018-07-22 00:22:44 +0530

    Final Commit

---
+++ AzureDownloadFile/AzureDownloadFile/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDownloadFile
+{
+    class Program
+    {
+        const string AccountName = "harikrishnan";
+        const string AccountKey = "CzLLQ7Noze4DNaWGEUy1wX9aDHP0685KOGliLsasaMaJfYJoRg11bfDtSVN7UrqZ3tHTYGyomSg/PUyl/s1EiA==";
+        static void Main(string[] args)
+        {
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(AccountName, AccountKey), true);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("newcontainer");
+            var blobs = container.ListBlobs();
+            DownloadBlobs(blobs);
+            Console.WriteLine("Download is Completed");
+        }
+        public static void DownloadBlobs(IEnumerable<IListBlobItem> blobs)
+        {
+            foreach (var file in blobs)
+            {
+                if (file is CloudBlockBlob blockBlob)
+                {
+                    blockBlob.DownloadToFileAsync(Path.Combine(@"C:\Users\hari.dileepkumar\Documents\Neudesic\DownloadFile", blockBlob.Name), FileMode.Create);
+                    Console.WriteLine(blockBlob.Name);
+                }
+                else if (file is CloudBlobDirectory blobDirectory)
+                {
+                    Directory.CreateDirectory(blobDirectory.Prefix);
+                    Console.WriteLine("Create Directory : " + blobDirectory.Prefix);
+                    DownloadBlobs(blobDirectory.ListBlobs());
+                }
+            }
+            
+        }
+    }
+}
---
+++ AzureUploadFile/AzureUploadFile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Azure;
+using System.IO;
+
+namespace AzureUploadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("newcontainer");
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("New.txt");
+            using (var fileStream = System.IO.File.OpenRead(@"C:\Users\hari.dileepkumar\Documents\Neudesic\UploadFile\New.txt"))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+        }
+    }
+}
